298c4d42ac6a80cc94c97a670d48c393e7cc65c0
inodentry <inodentry@users.noreply.github.com>
2023-09-18 10:10:18 +0000

    deploy: IyesGames/minewars@ffba8c145df1657b56870b6af50544252ed7c60b

--- minewars/api/minewars/ui/menu/sidebar-items.js
+++ minewars/api/minewars/ui/menu/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["menu_butt_interact_visual","spawn_menu_butt","spawn_menu_row","spawn_top_bar"],"mod":["mainmenu","offline"],"struct":["MenuButton","MenuContainer","MenuPlugin","MenuTitleText","MenuTopBar","MenuTopBarExtras"]};
+window.SIDEBAR_ITEMS = {"fn":["butt_back_showhide","cli_menu_back","menu_butt_interact_visual","spawn_menu_butt","spawn_menu_row","spawn_top_bar"],"mod":["mainmenu","offline"],"struct":["MenuBackButton","MenuButton","MenuContainer","MenuPlugin","MenuStack","MenuTitleText","MenuTopBar","MenuTopBarExtras"]};
